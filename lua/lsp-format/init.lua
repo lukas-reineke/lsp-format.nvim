f967bfbd0cb5322512acba53583086252895241c
Lukas Reineke <lukas.reineke@protonmail.com>
2022-04-18 12:32:25 +0900

    Version 2.2.2
    Don't overwrite the global format handler

--- lua/lsp-format/init.lua
+++ lua/lsp-format/init.lua
@@ -12,35 +12,6 @@ M.setup = function(format_options)
     vim.cmd [[command! -nargs=? -complete=filetype -bar FormatToggle lua require'lsp-format'.toggle(<q-args>)]]
     vim.cmd [[command! -nargs=? -complete=filetype -bar FormatDisable lua require'lsp-format'.disable(<q-args>)]]
     vim.cmd [[command! -nargs=? -complete=filetype -bar -bang FormatEnable lua require'lsp-format'.enable(<q-args>, "<bang>" == "!")]]
-
-    vim.lsp.handlers["textDocument/formatting"] = function(err, result, ctx)
-        if err ~= nil then
-            local client = vim.lsp.get_client_by_id(ctx.client_id)
-            local client_name = client and client.name or string.format("client_id=%d", ctx.client_id)
-            vim.api.nvim_err_write(string.format("%s: %d: %s", client_name, err.code, err.message))
-            return
-        end
-        if
-            result == nil
-            or vim.api.nvim_buf_get_var(ctx.bufnr, "format_changedtick") ~= vim.api.nvim_buf_get_var(
-                ctx.bufnr,
-                "changedtick"
-            )
-            or vim.startswith(vim.api.nvim_get_mode().mode, "i")
-        then
-            return
-        end
-
-        local view = vim.fn.winsaveview()
-        vim.lsp.util.apply_text_edits(result, ctx.bufnr, "utf-16")
-        vim.fn.winrestview(view)
-        if ctx.bufnr == vim.api.nvim_get_current_buf() then
-            vim.b.format_saving = true
-            vim.cmd [[update]]
-            vim.b.format_saving = false
-        end
-        M._next()
-    end
 end
 
 M.format = function(format_options_string)
@@ -79,24 +50,6 @@ M.format = function(format_options_string)
     M._next()
 end
 
-M._format = function(bufnr, client, format_options)
-    vim.b.format_changedtick = vim.b.changedtick
-    local params = vim.lsp.util.make_formatting_params(format_options)
-    client.request("textDocument/formatting", params, nil, bufnr)
-end
-
-M._next = function()
-    local next = M.queue[1]
-    if not next or #next.clients == 0 then
-        return
-    end
-    local next_client = table.remove(next.clients, 1)
-    M._format(next.bufnr, next_client, next.format_options)
-    if #next.clients == 0 then
-        table.remove(M.queue, 1)
-    end
-end
-
 M.disable = function(filetype)
     if filetype == "" then
         M.disabled = true
@@ -132,6 +85,53 @@ M.on_attach = function(client)
             autocmd BufWritePost <buffer> lua require'lsp-format'.format()
             augroup END
         ]]
+    end
+end
+
+M._handler = function(err, result, ctx)
+    if err ~= nil then
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        local client_name = client and client.name or string.format("client_id=%d", ctx.client_id)
+        vim.api.nvim_err_write(string.format("%s: %d: %s", client_name, err.code, err.message))
+        return
+    end
+    if
+        result == nil
+        or vim.api.nvim_buf_get_var(ctx.bufnr, "format_changedtick") ~= vim.api.nvim_buf_get_var(
+            ctx.bufnr,
+            "changedtick"
+        )
+        or vim.startswith(vim.api.nvim_get_mode().mode, "i")
+    then
+        return
+    end
+
+    local view = vim.fn.winsaveview()
+    vim.lsp.util.apply_text_edits(result, ctx.bufnr, "utf-16")
+    vim.fn.winrestview(view)
+    if ctx.bufnr == vim.api.nvim_get_current_buf() then
+        vim.b.format_saving = true
+        vim.cmd [[update]]
+        vim.b.format_saving = false
+    end
+    M._next()
+end
+
+M._format = function(bufnr, client, format_options)
+    vim.b.format_changedtick = vim.b.changedtick
+    local params = vim.lsp.util.make_formatting_params(format_options)
+    client.request("textDocument/formatting", params, M._handler, bufnr)
+end
+
+M._next = function()
+    local next = M.queue[1]
+    if not next or #next.clients == 0 then
+        return
+    end
+    local next_client = table.remove(next.clients, 1)
+    M._format(next.bufnr, next_client, next.format_options)
+    if #next.clients == 0 then
+        table.remove(M.queue, 1)
     end
 end
 
